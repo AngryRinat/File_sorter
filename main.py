205a7ff0b0b0dfa113d4e36a7e517dfd7fbd2b2c
Rinoreinz <angrykeo@gmail.com>
2022-11-07 23:12:19 +0300

    added minor things

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os, shutil
+import os, shutil, sys
 
 
 def moving_files():
-    path =
+    path = "/home/rinoreinz/example"
     list_of_files = []
     folder_set = set()
-    for root, dirs, files in os.walk("/home/rinoreinz/example"):
+    for root, dirs, files in os.walk(path):
         list_of_files += files
 
     for el in list_of_files:
@@ -15,28 +15,34 @@ def moving_files():
 
 
     for el in folder_set:
-        if not os.path.exists(f"/home/rinoreinz/example/{str(el)}"):
-             os.mkdir(f"/home/rinoreinz/example/{str(el)}")
+        if not os.path.exists(f"{path}/{str(el)}"):
+             os.mkdir(f"{path}/{str(el)}")
 
 
 
 
     list_of_files1 = []
-    for root, dirs, files in os.walk("/home/rinoreinz/example"):
+    for root, dirs, files in os.walk(path):
         list_of_files1 += files
 
     for file in list_of_files1:
         for set_name in folder_set:
-            if set_name in file and os.path.exists(f"/home/rinoreinz/example/{file}"):
-             shutil.copyfile(f"/home/rinoreinz/example/{file}", f"/home/rinoreinz/example/{set_name}/{file}")
+            if set_name in file and os.path.exists(f"{path}/{file}"):
+             shutil.copyfile(f"{path}/{file}", f"{path}/{set_name}/{file}")
 
     for file in list_of_files1:
-        if os.path.exists(f"/home/rinoreinz/example/{file}"):
-            os.remove(f"/home/rinoreinz/example/{file}")
+        if os.path.exists(f"{path}/{file}"):
+            os.remove(f"{path}/{file}")
 
 
 
 
 if __name__ == '__main__':
+
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+
     moving_files()
 
