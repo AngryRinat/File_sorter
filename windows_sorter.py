261410decfe9b667e5488785dc7073b89b76715c
Shchekatya <shchekatya@gmail.com>
2022-11-08 00:03:33 +0300

    fixed delete files

--- windows_sorter.py
+++ windows_sorter.py
@@ -3,7 +3,7 @@ import os, shutil, sys
 
 def moving_files(path):
 
-    folder_list = ["alina delphi", "ungaro motive", "cilium beige"]
+    folder_list = ["alina delphi", "unagro motive", "cilium beige"]
     list_of_files1 = []
     for root, dirs, files in os.walk(path):
         list_of_files1 += files
@@ -12,11 +12,13 @@ def moving_files(path):
         for set_name in folder_list:
             if set_name in file and os.path.exists(f"{path}/{file}"):
              shutil.copyfile(f"{path}/{file}", f"{path}/{set_name}/{file}")
+             if os.path.exists(f"{path}/{file}") and os.path.basename(f"{path}/{file}") != 'windows_sorter.exe':
+                 os.remove(f"{path}/{file}")
 
-    for file in list_of_files1:
-        print(os.path.basename(f"{path}/{file}"))
-        if os.path.exists(f"{path}/{file}") and os.path.basename(f"{path}/{file}") != 'windows_sorter.exe':
-            os.remove(f"{path}/{file}")
+    # for file in list_of_files1:
+    #     print(os.path.basename(f"{path}/{file}"))
+    #     if os.path.exists(f"{path}/{file}") and os.path.basename(f"{path}/{file}") != 'windows_sorter.exe':
+    #         os.remove(f"{path}/{file}")
 
 
 
@@ -28,5 +30,5 @@ if __name__ == '__main__':
     elif __file__:
         application_path = os.path.dirname(__file__)
 
-    moving_files(application_path)
+    moving_files('E:\Ринат\sorter')
 
