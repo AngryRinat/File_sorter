700eae02fb54f5136c14c86ea5890e9f480800e8
Shchekatya <shchekatya@gmail.com>
2022-11-10 21:57:29 +0300

    added some functionality

--- windows_sorter.py
+++ windows_sorter.py
@@ -6,11 +6,13 @@ def moving_files(path):
     folder_list = ["alina delphi", "unagro motive", "cilium beige"]
     list_of_files1 = []
     for root, dirs, files in os.walk(path):
-        list_of_files1 += files
+        if root == path:
+            list_of_files1 += files
+
 
     for file in list_of_files1:
         for set_name in folder_list:
-            if set_name in file and os.path.exists(f"{path}/{file}"):
+            if set_name in file and os.path.exists(f"{path}/{file}") :
              shutil.copyfile(f"{path}/{file}", f"{path}/{set_name}/{file}")
              if os.path.exists(f"{path}/{file}") and os.path.basename(f"{path}/{file}") != 'windows_sorter.exe':
                  os.remove(f"{path}/{file}")
@@ -19,7 +21,7 @@ def moving_files(path):
     #     print(os.path.basename(f"{path}/{file}"))
     #     if os.path.exists(f"{path}/{file}") and os.path.basename(f"{path}/{file}") != 'windows_sorter.exe':
     #         os.remove(f"{path}/{file}")
-
+    #
 
 
 
